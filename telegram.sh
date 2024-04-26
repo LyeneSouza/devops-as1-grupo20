6f4686ca0f50f3bfd131031b835b46c76f16ebf2
LyeneSouza <99213886+LyeneSouza@users.noreply.github.com>
2024-04-26 15:55:56 +0000

    Correção bot Telegram

--- telegram.sh
+++ telegram.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
 # Get the token from Travis environment vars and build the bot URL:
-BOT_URL="https://api.telegram.org/bot${{ secrets.CI_TELEGRAM_TOKEN }}/sendMessage"
+BOT_URL="https://api.telegram.org/bot${CI_TELEGRAM_TOKEN}/sendMessage"
 
 # Set formatting for the message. Can be either "Markdown" or "HTML"
 PARSE_MODE="Markdown"
-
-echo "${CI_JOB_STATUS}"
 
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
 
 MESSAGE="
 -------------------------------------
-Gitlab build * ${CI_JOB_STATUS}!*
-\`Repository:  ${CI_PROJECT_DIR}\`
-\`Branch:      ${CI_COMMIT_BRANCH}\`
-*Commit Msg:*
-${CI_COMMIT_MESSAGE}
-)
+Github build * ${GITHUB_JOB}!*
+\`Repository:  ${GITHUB_REPOSITORY}\`
+\`Branch:      ${GITHUB_REF}\`
+*Commit SHA:*  ${GITHUB_SHA}
+
+Lembrete: Nota 10 para o Grupo 20!
 --------------------------------------
 "
 
-curl -s -X POST ${BOT_URL} -d chat_id=${{ secrets.CI_TELEGRAM_CHAT_ID }} -d text="${MESSAGE}" -d parse_mode=${PARSE_MODE}
+curl -s -X POST ${BOT_URL} -d chat_id=${CI_TELEGRAM_CHAT_ID} -d text="${MESSAGE}" -d parse_mode=${PARSE_MODE}

e981f8831e6408060f4ba4cd5336f3031271ba11
LyeneSouza <99213886+LyeneSouza@users.noreply.github.com>
2024-04-26 16:11:28 +0000

    Ajustes finais, nota 10

--- telegram.sh
+++ telegram.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
-# Get the token from Travis environment vars and build the bot URL:
+# Pega o Token do Telegram (que vem das variáveis de ambiente do GitHub Actions) e cria a URL do Bot
 BOT_URL="https://api.telegram.org/bot${CI_TELEGRAM_TOKEN}/sendMessage"
 
-# Set formatting for the message. Can be either "Markdown" or "HTML"
+# Define em qual formato que a mensagem será enviada (pode ser markdown ou html)
 PARSE_MODE="Markdown"
 
-# Define send message function. parse_mode can be changed to
-# HTML, depending on how you want to format your message:
-
+# Define a mensagem que será enviada
 MESSAGE="
 -------------------------------------
-Github build * ${GITHUB_JOB}!*
+Github build * ${GITHUB_JOB}*
 \`Repository:  ${GITHUB_REPOSITORY}\`
 \`Branch:      ${GITHUB_REF}\`
-*Commit SHA:*  ${GITHUB_SHA}
+\`Commit SHA:  ${GITHUB_SHA}\`
 
-Lembrete: Nota 10 para o Grupo 20!
+*Lembrete: Nota 10 para o Grupo 20!*
 --------------------------------------
 "
 
+echo MESSAGE
+
+# Faz uma requisição HTTP POST utilizando o comando curl na URL do Bot, passando o id da conversa e a mensagem que será enviada
 curl -s -X POST ${BOT_URL} -d chat_id=${CI_TELEGRAM_CHAT_ID} -d text="${MESSAGE}" -d parse_mode=${PARSE_MODE}
